a9e0c4299841c2493c10c7d233bd2299f79bb753
acsnow99 <acsnow99@gmail.com>
2024-02-19 21:56:03 -0700

    Buttons can change images

--- game_buttons.js
+++ game_buttons.js
@@ -1,2 +1,9 @@
-console.log("Javascript loaded");
-document.querySelector("#game_button_run")?.addEventListener('click', function(event) { console.log("button clicked"); });
+console.log("Game buttons script loaded");
+monster_image_index = 0;
+document.querySelector("#game_button_run")?.addEventListener('click', function(event) { 
+    console.log("run button clicked"); 
+    monster_img_obj = document.querySelector("#monster_image");
+    monster_img_src = ["images/monster.jpg", "images/monster1.jpeg"];
+    monster_image_index = (monster_image_index + 1) % 2;
+    monster_img_obj.src = monster_img_src[monster_image_index];
+});
