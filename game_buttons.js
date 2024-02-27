13dc82bfb791ade42038ade1a7ed0dcf1d9b7d46
acsnow99 <acsnow99@gmail.com>
2024-02-27 11:20:00 -0700

    Game log works

--- game_buttons.js
+++ game_buttons.js
@@ -1,12 +1,13 @@
 console.log("Game buttons script loaded");
 
-monster_image_index = 0;
+let monster_index = 0;
 const local_username = localStorage.getItem("username");
 const score_access_string = "score" + local_username;
 const healing_access_string = "healing" + local_username;
 const health_access_string = "health" + local_username;
 const enemy_health_access_string = "enemy_health" + local_username;
 const monster_img_src = ["images/monster.jpg", "images/monster1.jpeg"];
+const monster_names = ["zombie", "landsquid"];
 const run_cost = -200;
 const base_win_score = 200;
 const win_score_multiplier = 52;
@@ -45,13 +46,9 @@ function update_healing_count(count) {
 function update_health_count(count) {
     const local_health = localStorage.getItem(health_access_string);
     let new_health = Number(local_health) + count;
-    localStorage.setItem(health_access_string, new_health);
-    update_health_display();
-}
-function update_health_count(count) {
-    const local_health = localStorage.getItem(health_access_string);
-    let new_health = Number(local_health) + count;
     if (new_health <= 0) {
+        alert("You were defeated by a " + monster_names[monster_index] + "! You lost " + String(lose_cost) + "g.");
+        update_log_display(local_username + " was defeated by a " + monster_names[monster_index] + " and lost " + String(lose_cost) + "g!");
         console.log("player lost");
         new_health = 3;
         update_score_count(lose_cost);
@@ -72,7 +69,10 @@ function update_enemy_health_count(count, player_gets_score_for_win=true) {
         const local_enemy_health_max = localStorage.getItem(enemy_health_access_string + "max");
         localStorage.setItem(enemy_health_access_string + "max", new_enemy_health);
         if (player_gets_score_for_win) {
-            update_score_count(base_win_score + win_score_multiplier * local_enemy_health_max);
+            const score_win = base_win_score + win_score_multiplier * local_enemy_health_max;
+            const message = local_username + " defeated a " + monster_names[monster_index] + " and earned " + String(score_win) + "g!";
+            update_log_display(message);
+            update_score_count(score_win);
         }
         update_monster_image();
     }
@@ -93,7 +93,7 @@ function update_score_count(count) {
 }
 function update_monster_image() {
     const monster_img_obj = document.querySelector("#monster_image");
-    monster_image_index = (monster_image_index + 1) % 2;
-    monster_img_obj.src = monster_img_src[monster_image_index];
+    monster_index = (monster_index + 1) % 2;
+    monster_img_obj.src = monster_img_src[monster_index];
 }
 
