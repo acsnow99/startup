382c757c802949d23d57ac7050d1ff02f5f581cd
acsnow99 <acsnow99@gmail.com>
2024-02-27 10:26:10 -0700

    Healing with heal button works as intended

--- game_buttons.js
+++ game_buttons.js
@@ -1,6 +1,10 @@
 console.log("Game buttons script loaded");
 
 monster_image_index = 0;
+const local_username = localStorage.getItem("username");
+const score_access_string = "score" + local_username;
+const healing_access_string = "healing" + local_username;
+const health_access_string = "health" + local_username;
 
 document.querySelector("#game_button_run")?.addEventListener('click', function(event) { 
     console.log("run button clicked");
@@ -11,9 +15,6 @@ document.querySelector("#game_button_run")?.addEventListener('click', function(e
 
 
     const run_cost = 200;
-
-    const local_username = localStorage.getItem("username");
-    const score_access_string = "score" + local_username;
     const local_score = localStorage.getItem(score_access_string);
     let new_score = local_score - run_cost;
     if (new_score < 0) {
@@ -22,10 +23,30 @@ document.querySelector("#game_button_run")?.addEventListener('click', function(e
     localStorage.setItem(score_access_string, new_score);
     update_score_display();
 
-    const healing_access_string = "healing" + local_username;
+    update_healing_count(1);
+});
+
+document.querySelector("#game_button_heal")?.addEventListener('click', function(event) {
+    console.log("heal button clicked");
+    if (!health_is_at_max()) {
+        update_healing_count(-1);
+        update_health_count(1);
+    }
+});
+
+function update_healing_count(count) {
     const local_healing = localStorage.getItem(healing_access_string);
-    let new_healing = Number(local_healing) + 1;
+    let new_healing = Number(local_healing) + count;
     localStorage.setItem(healing_access_string, new_healing);
     update_healing_display();
-});
+}
+function update_health_count(count) {
+    const local_health = localStorage.getItem(health_access_string);
+    let new_health = Number(local_health) + count;
+    localStorage.setItem(health_access_string, new_health);
+    update_health_display();
+}
+function health_is_at_max() {
+    return localStorage.getItem(health_access_string) >= 3;
+}
 
