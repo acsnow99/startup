85f3413e478a9678f5f2c1441d73019b64d75b78
acsnow99 <acsnow99@gmail.com>
2024-03-09 11:24:58 -0700

    Username taken message

--- public/login.js
+++ public/login.js
@@ -34,6 +34,13 @@ submit_login_and_advance_to_play = async function(event) {
         console.log("Sent username to server");
         window.location.href = "play.html";
     } catch (error) {
+        let username_parent = document.querySelector("#login_form");
+        let username_taken_element = document.createElement("p");
+        username_taken_element.className = "username_taken_message";
+        username_taken_element.id = "username_taken_message";
+        username_taken_element.innerText = "Hero name already taken...";
+        username_parent.appendChild(username_taken_element);
+
         console.log(error.message);
     }
 }
