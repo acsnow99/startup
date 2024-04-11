4a6e8339b69a47d801e51095e54fca376c4e5675
acsnow99 <acsnow99@gmail.com>
2024-04-11 15:06:58 -0600

    Bootstrap and css imported and render

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,24 @@
-import { useState } from 'react'
-import './App.css'
+import './style.css'
 import { Login } from "./Login";
+import "./bootstrap/css/bootstrap.css";
+
 
 function App() {
 
   return (
     <>
       <header>
-        <nav class="navbar navbar-expand navbar-dark">
-          <a class="navbar-brand" href="index.html">Cool CS RPG - </a>
-          <ul class="navbar-nav navbar-right">
-            <li class="nav-item">
-              <a class="nav-link" href="play.html">Play</a>
+        <nav className="navbar navbar-expand navbar-dark">
+          <a className="navbar-brand" href="index.html">Cool CS RPG - </a>
+          <ul className="navbar-nav navbar-right">
+            <li className="nav-item">
+              <a className="nav-link" href="play.html">Play</a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="rules.html">Rules</a>
+            <li className="nav-item">
+              <a className="nav-link" href="rules.html">Rules</a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="index.html">Logout</a>
+            <li className="nav-item">
+              <a className="nav-link" href="index.html">Logout</a>
             </li>
           </ul>
         </nav>
@@ -25,9 +26,7 @@ function App() {
 
       <Login />
 
-      <footer style="align-items: start">
-        <p>by Alex Snow - <a href="https://github.com/acsnow99/startup">GitHub</a></p>
-      </footer>
+      
     </>
   )
 }
