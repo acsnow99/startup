0693d683262019155349ad7d2db37aa33de050fa
acsnow99 <acsnow99@gmail.com>
2024-04-12 20:30:34 -0600

    Format looks a little better

--- src/Play.jsx
+++ src/Play.jsx
@@ -29,10 +29,10 @@ function Play() {
               <div className="game_buttons_container">
                 <button type="button" id="game_button_fight" onClick={player_attack} className="game_button_fight btn btn-primary btn-lg">Fight!</button>
                 <div className="game_buttons_container_internal">
-                  <button type="button" id="game_button_heal" onClick={player_heal} className="game_button_heal btn btn-success"><div id="healing_display">Heal! 0</div></button>
+                  <button type="button" id="game_button_heal" onClick={player_heal} className="game_button_heal btn btn-success"><img className="icon_heal" src="images/icon_heal_empty.png"/><div id="healing_display">Heal! 0</div></button>
                   <div>
                     <button type="button" id="game_button_run" onClick={player_run} className="game_button_run btn btn-secondary btn-sm">
-                      +1<img className="icon_heal" src="images/icon_heal.png"/>
+                      +1<img className="icon_heal" src="images/icon_heal_empty.png"/>
                       <div id="run_display">Run!</div>
                       -200<img className="icon_gold" src="images/icon_gold.png"/>
                     </button>
@@ -46,6 +46,7 @@ function Play() {
             </div>
           </div>
           <div className="game_log_container" id="game_log_container">
+            <div>Adventure Log:</div>
           </div>
         </div>
       </main>
